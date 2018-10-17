560e285edeed896388e2a17fc12000901ee54253
Artur Kuznetsov <artkuznetsov191@gmail.com>
2018-10-17 10:30:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+npm i (install dep)
+gulp (run tasks "build", "watch")
+gulp clean (del dir "build")
---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+
+//= ../../node_modules/jquery/dist/jquery.js
+//= ../../node_modules/bootstrap/dist/js/bootstrap.min.js
+
+
+
+// Custom
+
+//= components/app.js
